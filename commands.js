d7dca64b0237aa4e7b2e70d30037364464c0830c
Mike <michael@gagliano.me>
2018-09-04 00:24:44 -0400

    construction table total; partially working needs to be manually triggered

--- commands.js
+++ commands.js
@@ -18,16 +18,53 @@ for(var span in spans){
             var min = left * 10 % 60
             span.innerHTML = split[0] + " " + split[1] + " " + split[2] + " " + split[3] + " " + hour + "h " + min + "m left"
         }
-
     }
 }
+`;
+
+var tableTotaller = `
+var conButton = document.getElementById("ctl00_ContentPlaceHolder1_tabAssetStructurePBCConstruction")
+conButton.onclick = function(){tableGen();}
+function tableGen(){
+    var table = document.getElementById("ctl00_ContentPlaceHolder1_gvCorpHQStructureQueue");
+    var newRow = table.insertRow(-1);
+    var cell1 = newRow.insertCell(0);
+    for(var i=1; i<4;i++){
+        newRow.insertCell(i);
+    }
+    cell1.innerHTML = "<img src='images/icons/assets/asset_icon_10_22.gif' style='border-width:0px;border-width:0px; margin:0px 0px 0px 0px; padding:0px 0px 0px 0px;'>"
+    var cellCost = newRow.insertCell(4);
+    var cellTime = newRow.insertCell(5);
+    var rows = table.getElementsByTagName('td')
+    var totalCost = 0;
+    var totalTime = 0;
+    for(row in rows){
+        row = rows[row]
+        if(row.innerHTML){
+            
+            if(row.innerHTML.indexOf("$") > -1 && row.innerHTML.indexOf("<") == -1){
+                console.log(row.innerHTML)
+                totalCost += Number(row.innerHTML.replace("$","").replace(",",""))
+            } else if(Number(row.innerHTML)){
+                totalTime += Number(row.innerHTML)
+            }
+        }
+
+    }
+    totalTime = Math.floor((totalTime * 10) / 60) + "h " + ((totalTime * 10) % 60) + "m"
+    cellCost.style.textAlign = "right"
+    cellCost.innerHTML = "$" + totalCost.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    cellTime.style.textAlign = "center"
+    cellTime.innerHTML = totalTime;
+}
+
 `;
 
 var refreshTimer = `
 var checkTurn = function(){
     var time = document.getElementById("GameTurnCountdown").innerHTML;
     if(time == "00:00"){
-        setTimeout(function(){location.reload()}, 12500)
+        setTimeout(function(){location.reload()}, 13000)
     }
 }
 setInterval(checkTurn, 5000)
@@ -41,6 +78,10 @@ function runOnce(){
     var turnScript = document.createElement("script");
     turnScript.textContent = turnConvert;
     (document.head || document.documentElement).appendChild(turnScript);
+
+    var tableScript = document.createElement("script");
+    turnScript.textContent = tableTotaller;
+    (document.head || document.documentElement).appendChild(tableScript);
 }
 window.onload = function(){
     runOnce()
