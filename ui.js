eaf7e0913b196f376fbc3be7e9fbe12a4ad5c389
Mike <michael@gagliano.me>
2018-09-05 23:30:38 -0400

    basic extra notification at 30 seconds left of turn. thx @cmhull42

--- ui.js
+++ ui.js
@@ -6,7 +6,12 @@ function turnConvert(turns){
 
 function checkTurn(){
     var time = document.getElementById("GameTurnCountdown").innerHTML;
-    if(time == "00:00"){
+    if(time == "00:30"){
+        var warning = document.createElement("div")
+        warning.style = "position: fixed; z-index: 99;top: 15px; left: 15px; background-color: #C4C4C4; width: 250px; height: 25px; color: red;"
+        warning.innerHTML = "<b> This turn is ending in 30 seconds </b>"
+        document.body.appendChild(warning)
+    } else if(time == "00:00"){
         setTimeout(function(){location.reload()}, 15000)
     }
 }
