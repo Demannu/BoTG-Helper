fa0d7c50e66d14ee6f4859d34125270b5651e4a7
Mike <michael@gagliano.me>
2018-09-09 00:48:32 -0400

    gathered ids for viewscreen; implemented basic scraping of all but military

--- ui.js
+++ ui.js
@@ -12,7 +12,7 @@ function checkTurn(){
         warning.innerHTML = "<b> This turn is ending in 30 seconds </b>"
         document.body.appendChild(warning)
     } else if(time == "00:00"){
-        setTimeout(function(){location.reload()}, 15000)
+        setTimeout(function(){location.reload()}, (Math.floor((Math.random() * (200-150)) + 150)*100))
     }
 }
 
@@ -46,17 +46,110 @@ function scrapeDemand(){
     }
 }
 
-if(document.getElementById("ctl00_ContentPlaceHolder1_tabVSDemand")){
-    if(document.getElementById("ctl00_ContentPlaceHolder1_tabVSDemand").className == "hoverLeftNavTabON"){
-        if(document.getElementById("ctl00_ContentPlaceHolder1_rbDemandByProduct").checked){
-            scrapeDemand();
-        } else {
-            var parent = document.getElementById("ctl00_ContentPlaceHolder1_cbGridDemand").parentElement 
-            var infoSpan = document.createElement("span");
-            infoSpan.style = "color: orange;";
-            infoSpan.innerHTML = "<br>Change display to By Products"
-            var infoPanel = parent.appendChild(infoSpan)
+function scrapeIndustry(){
+    var planet = document.getElementById("ctl00_ContentPlaceHolder1_lblLeftNavHeading2").innerHTML;
+    var city = document.getElementById("ctl00_ContentPlaceHolder1_lblLeftNavHeading").innerHTML;
+    var demandTable = document.getElementById("ctl00_ContentPlaceHolder1_gvGridIndustryCategories");
+    for(var row in demandTable.rows){
+        row = demandTable.rows[row];
+        if(row.cells){
+            if(row.cells[2].innerHTML.length > 10){
+                var name = row.cells[0].innerHTML.split(">")[1].split("<")[0];
+                if(name !== "(return to top)"){
+                    var avg = row.cells[2].innerHTML.split(">")[1].split("<")[0];
+                    var val = row.cells[3].innerHTML;
+                    var vol = row.cells[4].innerHTML;
+                    var turn = document.getElementById("ctl00_lblCurrTurn").innerHTML;
+                    $.post("http://api.zvarpensg.xyz/industry", {
+                        planet: planet,
+                        city: city,
+                        resource: name,
+                        avg: avg,
+                        val: val,
+                        vol: vol,
+                        turn: Number(turn)
+                    })
+                }
+
+            }
         }
+    }
+}
+
+// Viewscreen (Parent): ctl00_tabViewscreen
+// Overview: ctl00_ContentPlaceHolder1_tabVSOverview
+//  Population: ctl00_ContentPlaceHolder1_lblOverviewPopulation
+//  Local Resources: ctl00_ContentPlaceHolder1_lblResCount
+//  Planet Resources: ctl00_ContentPlaceHolder1_lblResTotal
+//  Local Industry: ctl00_ContentPlaceHolder1_lblIndCount
+//  Planet Resources: ctl00_ContentPlaceHolder1_lblIndTotal
+//  Demand Average: ctl00_ContentPlaceHolder1_lblDemAVG
+//  Volume: ctl00_ContentPlaceHolder1_lblDemVOL
+//  Military Units: ctl00_ContentPlaceHolder1_lblMilCount
+//  Military Power: ctl00_ContentPlaceHolder1_lblMilPower
+// Resources: ctl00_ContentPlaceHolder1_tabVSResources
+// Industry: ctl00_ContentPlaceHolder1_tabVSIndustry
+// Demand: ctl00_ContentPlaceHolder1_tabVSDemand
+// Military: ctl00_ContentPlaceHolder1_tabVSMilitary
+
+
+if(document.getElementById("ctl00_tabViewscreen").className == "hoverTabON"){
+    var planet = document.getElementById("ctl00_ContentPlaceHolder1_lblLeftNavHeading2").innerHTML;
+    var city = document.getElementById("ctl00_ContentPlaceHolder1_lblLeftNavHeading").innerHTML;
+    var panels = {
+        "overview": "ctl00_ContentPlaceHolder1_tabVSOverview",
+        "resources": "ctl00_ContentPlaceHolder1_tabVSResources",
+        "industry": "ctl00_ContentPlaceHolder1_tabVSIndustry",
+        "demand": "ctl00_ContentPlaceHolder1_tabVSDemand",
+        "military": "ctl00_ContentPlaceHolder1_tabVSMilitary"
+    }
+    var activePanel = document.getElementsByClassName("hoverLeftNavTabON")[0].id.split("VS")[1].toLowerCase();
+
+    switch(activePanel){
+        case "overview":
+            break;
+        case "resources":
+            var resourceArray = [];
+            var resourceObject = {};
+            var parent = document.getElementById("ctl00_ContentPlaceHolder1_btnResByHex").parentElement.parentElement.parentElement.parentElement
+            parent.id="resourceTable"
+            $("table#resourceTable tr").each(function() {
+                var arrayOfThisRow = [];
+                var tableData = $(this).find('td');
+                if (tableData.length > 0) {
+                    tableData.each(function() { arrayOfThisRow.push($(this).text()); });
+                    resourceArray.push(arrayOfThisRow);
+                }
+            });
+            for(var i=2; i<resourceArray.length; i++){
+                resourceObject[resourceArray[i][0].replace(":","")] = resourceArray[i][1]
+            }
+            console.log(resourceObject);
+            break;
+        case "industry":
+            if(document.getElementById("ctl00_ContentPlaceHolder1_rbIndustryByProduct").checked){
+                scrapeIndustry();
+            } else {
+                var parent = document.getElementById("ctl00_ContentPlaceHolder1_cbGridIndustry").parentElement 
+                var infoSpan = document.createElement("span");
+                infoSpan.style = "color: orange;";
+                infoSpan.innerHTML = "<br>Change display to By Products"
+                var infoPanel = parent.appendChild(infoSpan)
+            }
+            break;
+        case "demand":
+            if(document.getElementById("ctl00_ContentPlaceHolder1_rbDemandByProduct").checked){
+                scrapeDemand();
+            } else {
+                var parent = document.getElementById("ctl00_ContentPlaceHolder1_cbGridDemand").parentElement 
+                var infoSpan = document.createElement("span");
+                infoSpan.style = "color: orange;";
+                infoSpan.innerHTML = "<br>Change display to By Products"
+                var infoPanel = parent.appendChild(infoSpan)
+            }
+            break;
+        case "military":
+            break;
     }
 }
 
